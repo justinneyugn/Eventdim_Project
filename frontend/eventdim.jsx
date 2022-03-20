0b5f19931a82dd859ef68a4978971f940f9f563f
Justin Nguyen <judnguyen@ucdavis.edu>
2022-03-20 14:59:36 -0700

    Add store to window

--- frontend/eventdim.jsx
+++ frontend/eventdim.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { postUser, postSession, deleteSession } from './util/session_api_util'
+import { postUser, postSession, deleteSession } from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
+    const store = configureStore();
     ReactDOM.render(<h1>Welcome to Eventdim!</h1>, root);
 
     window.postUser = postUser;
     window.postSession = postSession;
     window.deleteSession = deleteSession;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
 });
