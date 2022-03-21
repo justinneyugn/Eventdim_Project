b695b8979a178e17ed0db1cab1a9ab4ea9208ef3
Justin Nguyen <judnguyen@ucdavis.edu>
2022-03-21 16:20:15 -0700

    Add heroku

--- frontend/eventdim.jsx
+++ frontend/eventdim.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from './store/store';
 import Root from "./components/root";
+import { postUser, postSession, deleteSession } from './util/session_api_util'
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
@@ -11,4 +12,8 @@ document.addEventListener("DOMContentLoaded", () => {
     
     window.getState = store.getState;
     window.dispatch = store.dispatch;
+
+    window.postUser = postUser;
+    window.postSession = postSession;
+    window.deleteSession = deleteSession;
 });
