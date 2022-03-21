8b7a9ac6c2a28abd68d168edf14e583c60fe4a86
Justin Nguyen <judnguyen@ucdavis.edu>
2022-03-21 16:52:31 -0700

    Add jquery to application.js

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -15,7 +15,7 @@ class Api::SessionsController < ApplicationController
         @user = current_user
         if @user
             logout!
-            render "api/user/show"
+            render "api/users/show"
         else
             render json: ["Nobody signed in"], status: 404
         end
