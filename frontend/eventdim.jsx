3342105c402311bfdebea7deb53a70265e7b36a1
Justin Nguyen <judnguyen@ucdavis.edu>
2022-03-21 18:15:15 -0700

    Bootstrap current user

--- frontend/eventdim.jsx
+++ frontend/eventdim.jsx
@@ -6,7 +6,20 @@ import { postUser, postSession, deleteSession } from './util/session_api_util'
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    const store = configureStore();
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { id: window.currentUser.id }
+        };
+        window.preloadedState = preloadedState;
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
     ReactDOM.render(<Root store={store} />, root);
 
     
