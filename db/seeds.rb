5a360eb6dcb07744f0550be1110287c8c47a65c8
Justin Nguyen <judnguyen@ucdavis.edu>
2022-03-22 12:02:37 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,13 @@ event1 = Event.create(title: "Loud Festival",
     details: "Loud Festival is a three-day event, held at Cesar Chavez Park. This inaugural year, the festival will be headlined by one of the most famous rappers in the game, Rick Ross. This was no small feat for a new, relatively small music festival",
     date: Date.new(2022, 4, 24),
     creator_id: user1.id)
+
+event2 = Event.create(title: "Sacramento Grilled Cheese Festival 2022",
+    details: "The Sacramento Grilled Cheese Festival is a full weekend of events dedicated to the one food you never out grow.... GRILLED CHEESE! We've paired our grilled cheese with everything from craft beer to bounce houses so we've got something for everyone.",
+    date: Date.new(2022, 5, 15),
+    creator_id: user3.id)
+
+event3 = Event.create(title: "Muddy Princess Sacramento, CA",
+    details: "Our event is for Princesses of all sizes, ages and fitness levels. We don’t focus on fastest times or competitions, we focus on fun so be prepared to run, walk, crawl, climb, slide, balance and laugh your way along our course for an amazing “girls dirty day out!”",
+    date: Date.new(2022, 5, 14),
+    creator_id: user7.id)
