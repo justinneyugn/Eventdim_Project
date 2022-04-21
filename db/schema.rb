1bfa63c444a402c20ef093ecc067dc7746b306ab
Justin Nguyen <judnguyen@ucdavis.edu>
2022-04-20 18:54:28 -0700

    Add tickets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_31_043521) do
+ActiveRecord::Schema.define(version: 2022_04_21_014950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,16 @@ ActiveRecord::Schema.define(version: 2022_03_31_043521) do
     t.string "date", null: false
     t.string "category"
     t.index ["creator_id"], name: "index_events_on_creator_id"
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.integer "amount", null: false
+    t.integer "purchaser_id", null: false
+    t.integer "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_tickets_on_event_id"
+    t.index ["purchaser_id"], name: "index_tickets_on_purchaser_id"
   end
 
   create_table "users", force: :cascade do |t|
